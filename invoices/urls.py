9223f58b6c2a728614a44a161b043f6778f1db86
goldenjeffempire <jeffemuodafe124@gmail.com>
2025-10-21 17:08:45 +0100

    Add branded landing page

--- invoices/urls.py
+++ invoices/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('invoice/', views.invoice_create, name='invoice_create'),
+    path('', views.landing_page, name='landing_page'),
+    path('create/', views.invoice_create, name='invoice_create'),
     path('invoice/<int:pk>/', views.invoice_detail, name='invoice_detail'),
     path('invoice/<int:pk>/pdf/', views.invoice_pdf, name='invoice_pdf'),
 ]
