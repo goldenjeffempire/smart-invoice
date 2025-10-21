b6bcd59c1a7d6fafc7d9bf13416ee8aec886ccd1
goldenjeffempire <jeffemuodafe124@gmail.com>
2025-10-21 17:23:03 +0100

    Add root redirect to invoice form

--- smart_invoice/urls.py
+++ smart_invoice/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from invoices.views import landing_page
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('invoices/', include('invoices.urls')),
     path('', landing_page, name='landing'),
+    path('invoices/', include('invoices.urls')),
+    path('', RedirectView.as_view(url='/invoices/create/', permanent=False)),
 ]
