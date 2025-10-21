3e4e1e80e254200f01c6d4b209804a09569df540
goldenjeffempire <jeffemuodafe124@gmail.com>
2025-10-21 15:10:07 +0100

    Fixed & Update the dependencies tools & configuration files for production

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /data/data/com.termux/files/home/smart-invoice/venv
+set -gx VIRTUAL_ENV /home/jeffery/smart-invoice/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
